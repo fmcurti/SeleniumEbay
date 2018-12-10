769a3e7b39cc99fcb33c9b4be1d5954ba492c972
fcurti <fmcurti@gmail.com>
2018-12-10 13:11:29 -0300

    a

--- src/test/java/Tests/Tests.java
+++ src/test/java/Tests/Tests.java
@@ -25,7 +25,7 @@ public class Tests {
 	private WebDriver driver;
 	@BeforeMethod 
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
 		ChromeOptions ChromeOptions = new ChromeOptions();
 	    ChromeOptions.addArguments("-incognito");
 	    driver = new ChromeDriver();
