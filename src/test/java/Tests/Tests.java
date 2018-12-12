6d39a21fc81a97de719c7308ea89dbc4fdd6dbc3
fmcurti <fmcurti@gmail.com>
2018-12-12 13:43:56 -0300

    Update Tests.java

--- src/test/java/Tests/Tests.java
+++ src/test/java/Tests/Tests.java
@@ -13,8 +13,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.firefox.GeckoDriverService;
 import org.testng.annotations.Test;
 
 import pageObjects.EbayIndexPage;
